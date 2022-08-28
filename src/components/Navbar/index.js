cf45148706b81dc22e36a422cb5f513ff046abd2
dalyd14 <dalyd14@gmail.com>
2022-08-27 23:51:40 -0400

    fixing navlink bug

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,11 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { GiAmericanFootballHelmet } from 'react-icons/gi'
 
+import { useNavigate } from "react-router-dom";
+
 function NavigationBar({isLoggedin}) {
+
+    const navigate = useNavigate()
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -10,14 +14,14 @@ function NavigationBar({isLoggedin}) {
                 {
                     isLoggedin ?
                     <Nav className="me-auto">
-                        <Nav.Link href="/ballboys-offseason/submit-roster">Submit Roster</Nav.Link>
-                        <Nav.Link href="/ballboys-offseason/my-team">My Team</Nav.Link>
-                        <Nav.Link href="/ballboys-offseason/other-teams">Other Teams</Nav.Link>
+                        <Nav.Link onClick={() => {navigate("/submit-roster")}} >Submit Roster</Nav.Link>
+                        <Nav.Link onClick={() => {navigate("/my-team")}} >My Team</Nav.Link>
+                        <Nav.Link onClick={() => {navigate("/other-teams")}} >Other Teams</Nav.Link>
                     </Nav>
                     : <Nav className="me-auto"></Nav>
                 }
                 <Nav>
-                    <Nav.Link href="/ballboys-offseason/login">
+                    <Nav.Link onClick={() => {navigate("/login")}} >
                         <GiAmericanFootballHelmet size={'1.5rem'}/>
                     </Nav.Link>
                 </Nav>
