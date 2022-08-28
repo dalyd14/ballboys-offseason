1dcb53387d814032b88d27117002986e0acb0157
dalyd14 <dalyd14@gmail.com>
2022-08-27 23:44:32 -0400

    fixing navlink bug

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,18 +6,18 @@ function NavigationBar({isLoggedin}) {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Slowpoke Sports</Navbar.Brand>
+                <Navbar.Brand href="/ballboys-offseason">Slowpoke Sports</Navbar.Brand>
                 {
                     isLoggedin ?
                     <Nav className="me-auto">
-                        <Nav.Link href="/submit-roster">Submit Roster</Nav.Link>
-                        <Nav.Link href="/my-team">My Team</Nav.Link>
-                        <Nav.Link href="/other-teams">Other Teams</Nav.Link>
+                        <Nav.Link href="/ballboys-offseason/submit-roster">Submit Roster</Nav.Link>
+                        <Nav.Link href="/ballboys-offseason/my-team">My Team</Nav.Link>
+                        <Nav.Link href="/ballboys-offseason/other-teams">Other Teams</Nav.Link>
                     </Nav>
                     : <Nav className="me-auto"></Nav>
                 }
                 <Nav>
-                    <Nav.Link href="/login">
+                    <Nav.Link href="/ballboys-offseason/login">
                         <GiAmericanFootballHelmet size={'1.5rem'}/>
                     </Nav.Link>
                 </Nav>
